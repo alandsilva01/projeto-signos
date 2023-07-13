0468b1975dd6458448c9bc8988d4281f0b15765f
alandsilva01 <111643865+alandsilva01@users.noreply.github.com>
2023-07-12 23:20:02 -0300

    estilos dos signos

--- script.js
+++ script.js
@@ -6,31 +6,31 @@ function enviar() {
     var signo ="";
 
     if ((mes === 1 && dia >= 20) || (mes === 2 && dia <= 18)) {
-        signo = "Aquário";
+        signo = "➠Aquário♒";
       } else if ((mes === 2 && dia >= 19) || (mes === 3 && dia <= 20)) {
-        signo = "Peixes";
+        signo = "➠Peixes♓";
       } else if ((mes === 3 && dia >= 21) || (mes === 4 && dia <= 19)) {
-        signo = "Áries";
+        signo = "➠Áries♈";
       } else if ((mes === 4 && dia >= 20) || (mes === 5 && dia <= 20)) {
-        signo = "Touro";
+        signo = "➠Touro♉";
       } else if ((mes === 5 && dia >= 21) || (mes === 6 && dia <= 20)) {
-        signo = "Gêmeos";
+        signo = "➠Gêmeos♊";
       } else if ((mes === 6 && dia >= 21) || (mes === 7 && dia <= 22)) {
-        signo = "Câncer";
+        signo = "➠Câncer♋";
       } else if ((mes === 7 && dia >= 23) || (mes === 8 && dia <= 22)) {
-        signo = "Leão";
+        signo = "➠Leão♌";
       } else if ((mes === 8 && dia >= 23) || (mes === 9 && dia <= 22)) {
-        signo = "Virgem";
+        signo = "➠Virgem♍";
       } else if ((mes === 9 && dia >= 23) || (mes === 10 && dia <= 22)) {
-        signo = "Libra";
+        signo = "➠Libra♎";
       } else if ((mes === 10 && dia >= 23) || (mes === 11 && dia <= 21)) {
-        signo = "Escorpião♏";
+        signo = "➠Escorpião♏";
       } else if ((mes === 11 && dia >= 22) || (mes === 12 && dia <= 21)) {
-        signo = "Sagitário";
+        signo = "➠Sagitário♐";
       } else if ((mes === 12 && dia >= 22) || (mes === 1 && dia <= 19)) {
-        signo = "Capricórnio";
+        signo = "➠Capricórnio♑";
       } else {
-        signo = "Data de nascimento inválida.";
+        signo = "Data de nascimento inválida.☹";
       }
 
       document.getElementById("res").innerHTML = "Seu signo é:<span class='link-signo' onclick='redirecionar()'>" + signo + "</span>";
@@ -68,7 +68,7 @@ function redirecionar() {
     case "Libra":
       window.location.href = "libra.html";
       break;
-    case "Escorpião":
+    case "➠Escorpião♏":
       window.location.href = "escorpiao.html";
       break;
     case "Sagitário":
